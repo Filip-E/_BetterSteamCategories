4e09d33788c992471ab2cc1b29a26e37ba6a57cd
FilipElen <filipelengit@outlook.com>
2017-09-06 14:28:38 +0200

    removes hard coded steam API and ID. Adds replacement functionality(input text field with button)

--- Javascript/EventHandlers.js
+++ Javascript/EventHandlers.js
@@ -1,3 +1,9 @@
+function btnSendSteamInfoCLickListener(){
+  $('#btnSendSteamInfo').on('click', function(event) {
+    event.preventDefault();
+    getGame($('#txtSteamid').val(), $('#txtSteamAPIKey').val());
+  });
+}
 function btnCreateCategoryClickListener(){
   $('#btnCreateCategory').on('click', function(event) {
     // sends the new category to the server and if successfull add to the home.html
--- Javascript/GameList.js
+++ Javascript/GameList.js
@@ -1,9 +1,7 @@
 //game list functions
-function getGame(){
+function getGame(steamId, apiKey){
   var sUrl = 'http://localhost:8080/gamesList';
   // data which get send to the server
-  var apiKey = '6E8C398707989675705EC519EBB8790D';
-  var steamId = '76561198082201725';
   var keys = {
     'apiKey' : apiKey,
     'steamId' : steamId
@@ -25,6 +23,35 @@ function getGame(){
     dataType: 'json'
   });
 };
+// old getGame()
+/*
+function getGame(){
+  var sUrl = 'http://localhost:8080/gamesList';
+  // data which get send to the server
+  var apiKey = '[PASTE API KEY HERE BETWEEN QUOTES]';
+  var steamId = 'PASTE STEAM ID HERE BETWEEN QUOTES';
+  var keys = {
+    'apiKey' : apiKey,
+    'steamId' : steamId
+  };
+  $.ajax({
+    headers:{
+      'Accept':'application/json'
+    },
+    type: 'POST',
+    url: sUrl,
+    data: JSON.stringify(keys),
+    success: function(res){
+      console.log(res);
+      var gamesArray = res.response.games;
+      $.each(gamesArray,function(index, el) {
+        setHtmlGameList(index, el);
+      });
+    },
+    dataType: 'json'
+  });
+};
+*/
 
 function setHtmlGameList(htmlID, game){
   var appid = game.appid;
